f2d122be01435c4d9375d9b443fcc8a405aff6fd
laupierre <laupierre@users.noreply.github.com>
2023-05-02 11:32:11 +0200

    Create deg_unfiltered_analysis.R

---
+++ deg_unfiltered_analysis.R
@@ -0,0 +1,39 @@
+# 
+
+library (openxlsx)
+library (DESeq2)
+library (ggplot2)
+library (ggrepel)
+library (pheatmap)
+
+
+anno <- read.delim ("gencode.vM32.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+
+a <- read.delim ("subread.counts.txt", skip=1)
+a <- a[ ,grep ("ene|bam", colnames (a))]
+a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
+colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
+colnames (a) <- gsub ("star.IIT_RNM_", "", colnames (a))
+
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
+colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
+colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
+
+#write.xlsx (a, "star_gene_raw_counts.xlsx", rowNames=F)
+
+
+annot <- a
+annot <- annot[ ,c("Geneid", "gene_name", "gene_type", "mgi_id", "external_gene_name", "description")]
+
+
+torm <- c("gene_name", "gene_type", "mgi_id", "external_gene_name", "description")
+a <- a[ ,!colnames (a) %in% torm]
+row.names (a) <- a[ ,1]
+colnames (a) <- gsub ("star.IIT_X_", "", colnames (a))
+a <- a[ ,-1]
+
+
+
