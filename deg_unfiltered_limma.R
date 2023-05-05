c5ad7be63a93e4d44813a7b6be725a5f00329c57
laupierre <laupierre@users.noreply.github.com>
2023-05-05 15:43:29 +0200

    Update deg_unfiltered_limma.R

--- deg_unfiltered_limma.R
+++ deg_unfiltered_limma.R
@@ -1,1 +1,58 @@
+library (limma)
+library (openxlsx)
+
+
+anno <- read.delim ("gencode.v43.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+
+a <- read.delim ("subread.counts.txt", skip=1)
+a <- a[ ,grep ("ene|bam", colnames (a))]
+a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
+colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
+colnames (a) <- gsub ("star.IIT_RNM_", "", colnames (a))
+
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
+colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
+colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
+
+#write.xlsx (a, "star_gene_raw_counts.xlsx", rowNames=F)
+
+
+annot <- a
+annot <- annot[ ,c("Geneid", "gene_name", "gene_type", "hgnc_id", "external_gene_name", "description")]
+
+torm <- c("gene_name", "gene_type", "hgnc_id", "external_gene_name", "description")
+a <- a[ ,!colnames (a) %in% torm]
+row.names (a) <- a[ ,1]
+colnames (a) <- gsub ("star.", "", colnames (a))
+a <- a[ ,-1]
+
+
+pheno <- data.frame (matrix (nrow=dim (a)[2], ncol=2))
+colnames (pheno) <- c("sample", "genotype")
+row.names (pheno) <- pheno$sample <- colnames (a)
+pheno$genotype <- gsub ("_.*", "", pheno$sample)
+pheno$genotype [pheno$genotype == "PGBD5"] <- "shPGBD5" 
+
+
+
+## OE paired contrast
+pheno.s <- pheno[grep ("DOX", pheno$genotype), ]
+pheno.s
+
+pheno.s$batch <- factor (c(1,2,3,2,1,3)) 
+
+a.s <- a[ ,colnames (a) %in% pheno.s$sample]
+stopifnot (colnames (a.s) == pheno.s$sample)
+
+
+## limma chunk
+
+
+
+
+
+
 
